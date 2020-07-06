d22997301bde6f197c15f0a9aab731389238e09e
Diogo Reis <diogommreis1@gmail.com>
2020-07-06 15:50:02 +0100

    setting the routes

--- src/routes.tsx
+++ src/routes.tsx
@@ -2,20 +2,21 @@ import React from "react";
 import { Route, BrowserRouter } from "react-router-dom";
 
 import Home from "./pages/home";
-//import Contact from "./pages/contact";
-//import orderServices from "./pages/orderServices";
-//import rentalServices from "./pages/rentalServices";
-//import transferServices from "./pages/transferServices";
+import Contact from "./pages/contact";
+import orderServices from "./pages/orderServices";
+import rentalServices from "./pages/rentalServices";
+import transferServices from "./pages/transferServices";
 /* <Route component={Contact} path="/contact" exact />
-      <Route component={orderServices} path="/order" exact />
-      <Route component={rentalServices} path="/rentacar" exact />
-      <Route component={transferServices} path="/transfer" exact />
-    */
+ */
 
 const Routes = () => {
   return (
     <BrowserRouter>
       <Route component={Home} path="/" exact />
+      <Route component={Contact} path="/contact" exact />
+      <Route component={orderServices} path="/orderServices" exact />
+      <Route component={rentalServices} path="/rentalServices" exact />
+      <Route component={transferServices} path="/transferServices" exact />
     </BrowserRouter>
   );
 };
