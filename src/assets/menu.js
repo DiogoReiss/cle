753d121a9d1a03a2fa7d95bf0c7ef015761ffb6e
Diogo Reis <diogommreis1@gmail.com>
2020-07-06 15:50:45 +0100

    deleting few unused files and modifying some things

--- src/assets/menu.js
+++ src/assets/menu.js
@@ -1,17 +1,13 @@
-const menu = document.querySelector('#servicesLayout');
-const menuBackground = document.querySelector('#servicesMenu');
-const menuBackgroundDetail = document.querySelector('#menuDetail');
-const orderMenu = document.querySelector('#orderMenu');
-const rentalMenu = document.querySelector('#rentalMenu');
-const transferMenu = document.querySelector('#transferMenu');
 var click = 0;
+function menuScript() {
+  const menuBackground = document.querySelector("#servicesMenu");
 
-menu.addEventListener('click', () => {
-  console.log('Anyone clicked!');
-  menuBackground.classList.add('activated')
+  menuBackground.classList.add("activated");
   click += 1;
   if (click === 2) {
-    menuBackground.classList.remove('activated')
-    click = 0
+    menuBackground.classList.remove("activated");
+    click = 0;
   }
-})
+}
+
+export default menuScript;
